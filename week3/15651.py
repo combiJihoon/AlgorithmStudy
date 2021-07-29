e230afff875f6dc013b1419930bc77ebc442183d
combiJihoon <combi_jihoon@naver.com>
2021-07-29 14:29:20 +0900

    week3 15651

---
+++ week3/15651.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+stack = []
+
+
+def dfs():
+    if len(stack) == m:
+        print(" ".join(map(str, stack)))
+        return
+
+    for i in range(1, n+1):
+        stack.append(i)
+        dfs()
+        stack.pop()
+
+
+dfs()
