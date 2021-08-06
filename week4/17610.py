c82df033506dec7e401533fedd6fd405979c13e8
combiJihoon <combi_jihoon@naver.com>
2021-08-07 00:38:39 +0900

    week4 17610 update references

--- week4/17610.py
+++ week4/17610.py
@@ -32,3 +32,18 @@ def dfs(l, total):
 result = set()
 dfs(0, 0)
 print(s-len(result))
+
+
+'''
+참고2
+
+n = int(input())
+arr = [*map(int, input().split())]
+dp = {0}
+for a in arr:
+    temp = set()
+    for b in dp:
+        temp.update({max(0, b-a), max(0, a-b), b+a, b})
+    dp.update(temp)
+print(sum(arr) - len(dp) + 1)
+'''
