51246372122d5251168da0ea13de6b6142fc4b75
combiJihoon <combi_jihoon@naver.com>
2021-10-20 11:02:39 +0900

    Elice coding test3

---
+++ elice/test3-1.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+n, h = map(int, input().split())
+arr = sorted([list(map(int, input().split())) for _ in range(n)])
+
+stop, cnt = 0, 0
+for x, s, k in arr:
+    if s > h:
+        if k == 3:
+            stop = x
+            break
+        if k == 1:
+            cnt += 1
+else:
+    stop = -1
+
+print(stop, cnt)
