b1f33065f16b0157367d7e271099592d70b016b6
combiJihoon <combi_jihoon@naver.com>
2021-10-28 01:10:15 +0900

    Add a condition to the stack-sol and make a new sol using 'eval' method

--- data-structure/2504.py
+++ data-structure/2504.py
@@ -3,55 +3,81 @@ import sys
 input = sys.stdin.readline
 string = input().strip()
 
-stack = []
-for i in string:
-    if i == ')':
-        tmp = 0
-        while stack:
-            x = stack.pop()
-            if x == '(':
-                if tmp == 0:
-                    stack.append(2)
-                else:
-                    stack.append(2*tmp)
-                break
-            elif x == '[':
-                print(0)
-                sys.exit()
-            else:
-                if tmp == 0:
-                    tmp = int(x)
-                else:
-                    tmp += int(x)
+if list(string).count('[') != list(string).count(']') or list(string).count('(') != list(string).count(')'):
+    print(0)
+    sys.exit()
 
-    elif i == ']':
-        tmp = 0
-        while stack:
-            x = stack.pop()
-            if x == '[':
-                if tmp == 0:
-                    stack.append(3)
+string = string.replace('()', '+2').replace('[]', '+3').replace(
+    '(', '+(').replace('[', '+(').replace(')', ')*2').replace(']', ')*3')
+
+try:
+    print(eval(string))
+except:
+    print(0)
+
+
+'''
+# 스택으로 풀기
+
+import sys
+
+input = sys.stdin.readline
+string = input().strip()
+
+if list(string).count('[') != list(string).count(']') or list(string).count('(') != list(string).count(')'):
+    print(0)
+
+else:
+    stack = []
+    for i in string:
+        if i == ')':
+            tmp = 0
+            while stack:
+                x = stack.pop()
+                if x == '(':
+                    if tmp == 0:
+                        stack.append(2)
+                    else:
+                        stack.append(2*tmp)
+                    break
+                elif x == '[':
+                    print(0)
+                    sys.exit()
                 else:
-                    stack.append(3*tmp)
-                break
-            elif x == '(':
-                print(0)
-                sys.exit()
-            else:
-                if tmp == 0:
-                    tmp = int(x)
+                    if tmp == 0:
+                        tmp = int(x)
+                    else:
+                        tmp += int(x)
+
+        elif i == ']':
+            tmp = 0
+            while stack:
+                x = stack.pop()
+                if x == '[':
+                    if tmp == 0:
+                        stack.append(3)
+                    else:
+                        stack.append(3*tmp)
+                    break
+                elif x == '(':
+                    print(0)
+                    sys.exit()
                 else:
-                    tmp += int(x)
+                    if tmp == 0:
+                        tmp = int(x)
+                    else:
+                        tmp += int(x)
 
-    else:
-        stack.append(i)
+        else:
+            stack.append(i)
 
-ans = 0
-for i in stack:
-    if i == '(' or i == '[':
-        print(0)
-        sys.exit()
-    else:
-        ans += i
+    ans = 0
+    for i in stack:
+        if i == '(' or i == '[':
+            print(0)
+            sys.exit()
+        else:
+            ans += i
 
-print(ans)
+    print(ans)
+'''
